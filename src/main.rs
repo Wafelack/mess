6080abf0bc102236df30e50e1fccd5d8c060428f
Wafelack <wafelack@protonmail.com>
2020-11-13 22:15:15 +0100

    init: initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use colored::*;
+use lines_from_file::lines_from_file;
+
+fn main() {
+    let args: Vec<String> = std::env::args().into_iter().skip(1).collect();
+
+    if args.len() >= 2 {
+        if !std::path::Path::new(&args[1]).exists() {
+            eprintln!("File `{}` not found", &args[1]);
+            std::process::exit(-5);
+        }
+        let lines = lines_from_file(&args[1]);
+        let mut counter: usize = 0;
+        for line in lines {
+            counter += 1;
+            if line.contains(&args[0]) {
+                println!("{} | {}", counter, line);
+            }
+        }
+    } else if args.len() >= 2 {
+    }
+}
