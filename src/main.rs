fb6a8e281ce82cdac3aefa3bf5305e7e33c0a568
Wafelack <wafelack@protonmail.com>
2020-11-27 16:45:51 +0100

    feat: building recursive version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use clap::{Arg, App, SubCommand, ArgMatches};
 
 fn get_lines(filename: &str) -> Vec<String> {
@@ -12,11 +12,7 @@ fn get_lines(filename: &str) -> Vec<String> {
     };
     let stringed = match std::str::from_utf8(&content) {
         Ok(s) => s,
-        Err(e) => {
-            eprintln!("An error occured while converting file (probably cause it does not contain valid utf8)");
-            eprintln!("Debug info : {}", e);
-            std::process::exit(-2);
-        }
+        Err(e) => return vec![],
     };
     stringed.to_string().split('\n').into_iter().map(|s| {s.to_string()}).collect::<Vec<String>>()
 }  
@@ -26,6 +22,73 @@ fn is_specified(val: &str, matches: &ArgMatches) -> bool {
         return true;
     }
     false
+}
+fn single_file(fname: &Option<&str>, pattern: &str) {
+    if !Path::new(fname.unwrap()).exists() {
+        eprintln!("File not found");
+        std::process::exit(-1);
+    }
+    let mut all_lines: Vec<(String, usize)> = vec![];
+    let content = get_lines(fname.unwrap());
+    let mut i = 0usize;
+    for line in content  {
+        i+=1;
+        if line.contains(pattern) {
+            all_lines.push((line, i));
+        }            
+    }
+    for c in all_lines {
+        println!("{} | {} | {}", &fname.unwrap(), c.1, c.0);
+    }
+    std::process::exit(0);   
+}
+fn some_files(pattern: &str, recurse: bool) {
+    let entries = match see_dir(PathBuf::from("."), recurse) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("An error occured while reading dir");
+            eprintln!("Debug info : {}", e);
+            std::process::exit(-5);
+        }
+    };
+
+
+    let mut all_lines: Vec<(String, &str, usize)> = vec![];
+
+    for entry in &entries {
+        let fname = entry.as_path().as_os_str().to_str().unwrap(); 
+        let fcontent = get_lines(fname);
+        let mut i = 0usize;
+        for line in fcontent {
+            i+=1;
+            if line.contains(pattern) {
+                all_lines.push((line, fname, i));
+            }
+        }
+    }
+    for line in all_lines {
+        println!("{} | {} | {}", line.1, line.2, line.0);
+    }
+    std::process::exit(0);
+}
+fn see_dir<'a>(dir: PathBuf, recurse: bool) -> Result<Vec<PathBuf>, &'a str> {
+    let mut list: Vec<PathBuf> = Vec::new();
+    for entry in match std::fs::read_dir(dir.clone()) {Ok(e) => e, Err(_s) => return Err("Could not read dir")} {
+        let entry = match  entry {
+            Ok(e) => e,
+            Err(_e) => return Err("Could not convert to DirEntry"),
+        };
+        if entry.path().is_dir() && !recurse {
+            let mut sub: Vec<PathBuf> = Vec::new();
+            sub = see_dir(entry.path(), true)?;
+            list.extend(sub);
+        } else {
+            if !entry.path().is_dir() {
+                list.push(entry.path().to_owned());
+            }
+        }
+    }
+    Ok(list)
 }
 fn main() {
     let matches = App::new("sgrep")
@@ -53,22 +116,8 @@ fn main() {
     let fname = &matches.value_of("fname");
 
     if fname.is_some() {
-        if !Path::new(fname.unwrap()).exists() {
-            eprintln!("File not found");
-            std::process::exit(-1);
-        }
-        let mut all_lines: Vec<(String, usize)> = vec![];
-        let content = get_lines(fname.unwrap());
-        let mut i = 0usize;
-        for line in content  {
-            i+=1;
-            if line.contains(pattern) {
-                all_lines.push((line, i));
-            }            
-       }
-       for c in all_lines {
-            println!("{} | {} | {}", &fname.unwrap(), c.1, c.0);
-       }
-       std::process::exit(0);
+        single_file(fname, pattern);
+    } else {
+        some_files(pattern, recursive);
     }
 }
