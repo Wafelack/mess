b69e4213e6aa43c0914b4b84ba8e85555b32367e
Wafelack <wafelack@protonmail.com>
2020-12-14 07:24:44 +0100

    feat: turning into a library && added multi types

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 use sha2::{Digest, Sha256};
 use hex;
 use chrono::prelude::*;
-use std::fmt::Formatter;
+use std::fmt::{Debug, Formatter};
 
 #[derive(Debug, Clone)]
-struct Block {
+pub struct Block<T> where T: std::fmt::Debug, T: std::default::Default, T: std::clone::Clone {
     index: u32,
     time_stamp: String,
-    data: i32,
+    data: T,
     hash: String,
     prev_hash: String,
     nonce: String,
     difficulty: u32,
 }
 
-impl Block {
-    fn new(old_block: &Block, data: i32, dif: u32) -> Self {
+impl<T> Block<T> where T: std::fmt::Debug, T: std::default::Default, T: std::clone::Clone {
+    pub fn new(old_block: &Block<T>, data: T, dif: u32) -> Self {
         let index = old_block.index + 1;
         let t = Utc::now();
         let time_stamp = format!("{}", t);
@@ -35,14 +35,14 @@ impl Block {
         block
 
     }
-    fn is_block_valid(&self, old_block: &Block) -> bool {
+    pub fn is_block_valid(&self, old_block: &Block<T>) -> bool {
         if old_block.index + 1 != self.index { return false; }
         if old_block.hash != self.prev_hash { return false; }
         if self.calculate_hash() != self.hash { return false; }
         true
     }
-    fn calculate_hash(&self) -> String {
-        let record = format!("{}{}{}{}{}", self.index, self.time_stamp, self.data, self.prev_hash, self.nonce);
+    pub fn calculate_hash(&self) -> String {
+        let record = format!("{}{}{:?}{}{}", self.index, self.time_stamp, self.data, self.prev_hash, self.nonce);
         let mut h = Sha256::new();
         h.update(record.as_bytes());
         let hashed = h.finalize();
@@ -51,14 +51,14 @@ impl Block {
     }
 }
 
-impl std::fmt::Display for Blockchain {
+impl<T> std::fmt::Display for Blockchain<T> where T: std::fmt::Debug, T: std::default::Default, T: std::clone::Clone {
     fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result{
         println!("[");
         for block in &self.blocks {
             println!("  {{");
             println!("    \"Index\": {},", block.index);
             println!("    \"Timestamp\": \"{}\",", block.time_stamp);
-            println!("    \"Data\": {},", block.data);
+            println!("    \"Data\": {:?},", block.data);
             println!("    \"Hash\": \"{}\",", block.hash);
             println!("    \"PrevHash\": \"{}\"", block.prev_hash);
             if block.index as usize == self.blocks.len() - 1 {
@@ -73,22 +73,23 @@ impl std::fmt::Display for Blockchain {
     }
 }
 #[derive(Debug, Clone)]
-struct Blockchain {
-    blocks: Vec<Block>,
+pub struct Blockchain<T> where T: std::fmt::Debug, T: std::default::Default, T: std::clone::Clone {
+    blocks: Vec<Block<T>>,
     difficulty: u32,
 }
-impl Blockchain {
-    fn init(difficulty: u32) -> Self {
+impl<T> Blockchain<T> where T: std::fmt::Debug, T: std::default::Default, T: std::clone::Clone {
+    pub fn init(difficulty: u32, ) -> Self {
         let t = Utc::now();
-        let block = Block {index: 0, time_stamp: format!("{}", t), data: 0, hash: "".to_string(), prev_hash: "".to_string(), difficulty, nonce: "".to_string()};
+        let default: T = Default::default();
+        let block = Block {index: 0, time_stamp: format!("{}", t), data: default, hash: "".to_string(), prev_hash: "".to_string(), difficulty, nonce: "".to_string()};
         Self {blocks: vec![block], difficulty}
     }
-    fn replace_chain(&mut self, new_blocks: Vec<Block>) {
+    pub fn replace_chain(&mut self, new_blocks: Vec<Block<T>>) {
         if new_blocks.len() > self.blocks.len() {
             self.blocks = new_blocks;
         }
     }
-    fn add_block(&mut self, data: i32) {
+    pub fn add_block(&mut self, data: T) {
         if self.blocks.len() < 1 {
             return;
         }
@@ -102,7 +103,7 @@ impl Blockchain {
     }
 }
 
-fn is_hash_valid(hash: String, difficulty: u32) -> bool {
+pub fn is_hash_valid(hash: String, difficulty: u32) -> bool {
     let mut prefix = String::new();
     for _ in 0..difficulty {
         prefix.push('0')
@@ -110,11 +111,30 @@ fn is_hash_valid(hash: String, difficulty: u32) -> bool {
     hash.starts_with(&prefix)
 }
 
-fn main() {
-    let mut block_chain = Blockchain::init(4);
-    block_chain.add_block(5);
-    block_chain.add_block(6);
-    block_chain.add_block(7);
-    block_chain.add_block(8);
-    println!("{}", block_chain);
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn hashes() {
+        let mut block_chain = Blockchain::<bool>::init(1);
+        block_chain.add_block(true);
+        block_chain.add_block(false);
+        assert_eq!(block_chain.blocks[block_chain.blocks.len() - 1].prev_hash, block_chain.blocks[block_chain.blocks.len() - 2].hash);
+    }
+
+    #[test]
+    fn validation() {
+        let mut block_chain = Blockchain::init(1);
+        block_chain.add_block(5);
+        block_chain.add_block(6);
+        assert!(block_chain.blocks[block_chain.blocks.len() - 1].is_block_valid(&block_chain.blocks[block_chain.blocks.len() - 2]));
+    }
+    #[test]
+    fn display() {
+        let mut blockchain = Blockchain::<Vec<u8>>::init(1);
+        blockchain.add_block("Hello, World !".to_string().into_bytes());
+        blockchain.add_block("Goodbye !".to_string().into_bytes());
+        println!("{}", blockchain);
+    }
 }
