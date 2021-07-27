7f58e220b6a55b29e3ec850d0714fd9a44830776
Wafelack <wafelack@riseup.net>
2021-07-27 12:42:55 +0200

    scala: Remove useless functions.

--- scala/scalabf/scalabf/src/Main.scala
+++ scala/scalabf/scalabf/src/Main.scala
@@ -3,13 +3,7 @@ import scala.io.StdIn.{readLine, readChar};
 case class VmState(ip: Int, labels: Array[Int], memory: Array[Char], ptr: Int);
 
 def execute(code: String) = {
-  val mem: Array[Char] = Array(1);
-  runTurn(code, VmState(0, Array(), replaceAt(mem, 0, 0), 0));
-}
-def replaceAt(arr: Array[Char], idx: Int, v: Char): Array[Char] = {
-  return if (arr.isEmpty) arr
-  else if (idx == 0) v +: arr.tail
-  else arr.head +: replaceAt(arr.tail, idx - 1, v);
+  runTurn(code, VmState(0, Array(), Array[Char](1).updated(0, 0), 0));
 }
 private def runTurn(code: String, state: VmState): Unit = {
 	if (state.ip >= code.length) return;
@@ -17,15 +11,15 @@ private def runTurn(code: String, state: VmState): Unit = {
 	code.charAt(state.ip) match {
     case '>' => runTurn(code, VmState(state.ip + 1, state.labels, if (state.memory.length <= state.ptr + 1) state.memory :+ 0 else state.memory, state.ptr + 1));
     case '<' => runTurn(code, VmState(state.ip + 1, state.labels, state.memory, if (state.ptr == 0) state.memory.length - 1 else state.ptr - 1));
-    case '+' => runTurn(code, VmState(state.ip + 1, state.labels, replaceAt(state.memory, state.ptr, if (pointedVal == 255) 0 else (pointedVal + 1).toChar), state.ptr));
-    case '-' => runTurn(code, VmState(state.ip + 1, state.labels, replaceAt(state.memory, state.ptr, if (pointedVal == 0) 255 else (pointedVal - 1).toChar), state.ptr));
+    case '+' => runTurn(code, VmState(state.ip + 1, state.labels, state.memory.updated(state.ptr, if (pointedVal == 255) 0 else (pointedVal + 1).toChar), state.ptr));
+    case '-' => runTurn(code, VmState(state.ip + 1, state.labels, state.memory.updated(state.ptr, if (pointedVal == 0) 255 else (pointedVal - 1).toChar), state.ptr));
     case '[' => runTurn(code, VmState(state.ip + 1, state.labels :+ state.ip, state.memory, state.ptr));
     case ']' => runTurn(code, VmState(if (pointedVal > 0) state.labels.last else state.ip + 1, if (pointedVal == 0) state.labels.init else state.labels, state.memory, state.ptr));
     case '.' => {
       print(pointedVal);
       runTurn(code, VmState(state.ip + 1, state.labels, state.memory, state.ptr));
     }
-    case ',' => runTurn(code, VmState(state.ip + 1, state.labels, replaceAt(state.memory, state.ptr, readChar), state.ptr));
+    case ',' => runTurn(code, VmState(state.ip + 1, state.labels, state.memory.updated(state.ptr, readChar), state.ptr));
     case _ => runTurn(code, VmState(state.ip + 1, state.labels, state.memory, state.ptr)),
   }
 }
